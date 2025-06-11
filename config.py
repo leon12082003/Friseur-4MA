a9692c5937e8d083c170c408fd647b5c197f730c
leon12082003 <markus.bringer@gmx.de>
2025-06-11 23:00:21 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+# Friseure und ihre Google Kalender-IDs – HIER manuell eintragen!
+CALENDARS = {
+    "Lisa Fischer": "kalender-id-lisa@google.com",
+    "Marco Richter": "kalender-id-marco@google.com",
+    "Marie Zeiser": "kalender-id-marie@google.com",
+    "Max Herrmann": "kalender-id-max@google.com"
+}
+
+# Öffnungszeiten
+OPENING_HOURS = {
+    "Mon": ("09:00", "18:00"),
+    "Tue": ("09:00", "18:00"),
+    "Wed": ("09:00", "18:00"),
+    "Thu": ("09:00", "18:00"),
+    "Fri": ("09:00", "18:00"),
+    "Sat": ("09:00", "14:00")
+}
