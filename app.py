5624198a82c45e7c7b3c723420ccdcfd32581d92
leon12082003 <markus.bringer@gmx.de>
2025-06-11 23:24:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -162,4 +162,6 @@ def cancel():
     return jsonify({"success": True})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
