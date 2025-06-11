8f955562da17d4e090d5f53f8c7b19e3ecf3650c
leon12082003 <markus.bringer@gmx.de>
2025-06-11 22:23:18 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # Manuell anpassen: Trage hier die echten Kalender-IDs ein
 CALENDARS = {
-    "Lisa Fischer": "kalender-id-lisa@google.com",
-    "Marco Richter": "kalender-id-marco@google.com",
-    "Marie Zeiser": "kalender-id-marie@google.com",
-    "Max Herrmann": "kalender-id-max@google.com"
+    "Lisa Fischer": "c196fca542ff5176c62b11805596e015f5fb2aff1ec0b73c43cf555937911638@group.calendar.google.com",
+    "Marco Richter": "00edba1ec002f208f25bbd78a7649ef0518121d31071a4fed10ecc0393e3d4b9@group.calendar.google.com",
+    "Marie Zeiser": "1e3a743874cf760bf08026e4fd7eb4a2692c5996e8de1181fd8c87837be524e7@group.calendar.google.com",
+    "Max Herrmann": "5b679eaf3a0999b06265e6b21dd2200800e8b2338056475e1cbd8f17ab34f861@group.calendar.google.com"
 }
 
 # Öffnungszeiten
