cd9da6e14a167393539b069e1e12efd135b73872
leon12082003 <markus.bringer@gmx.de>
2025-06-12 14:30:20 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+
+calendar_ids = {
+    "Lisa Fischer": "c196fca542ff5176c621b1805596e015fb2affc1e0cb73c43cf555937911638@group.calendar.google.com",
+    "Marco Richter": "00eb0adec6022faf25bb0d7a7649ef05812d130171af4efd10ecc0393e34db9@group.calendar.google.com",
+    "Marie Zeiser": "1e3a7437847c760bf08026e4fd7eb4a2692c599e8de1181fd8c87837be524e7@group.calendar.google.com",
+    "Max Herrmann": "5b679eaf3a0999b0265e6b21dd202080e8b2338650475e1cbd8f17ab34f861@group.calendar.google.com"
+}
+
+OPENING_HOURS = {
+    "Mon": ("09:00", "18:00"),
+    "Tue": ("09:00", "18:00"),
+    "Wed": ("09:00", "18:00"),
+    "Thu": ("09:00", "18:00"),
+    "Fri": ("09:00", "18:00"),
+    "Sat": ("09:00", "14:00")
+}
