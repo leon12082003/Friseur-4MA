689986ca5945e870fa6b333c0e070b2b3784efb6
leon12082003 <markus.bringer@gmx.de>
2025-06-12 18:56:18 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 CALENDARS = {
-    "lisa": "REPLACE_WITH_LISA_CALENDAR_ID",
-    "marco": "REPLACE_WITH_MARCO_CALENDAR_ID",
-    "marie": "REPLACE_WITH_MARIE_CALENDAR_ID",
-    "max": "REPLACE_WITH_MAX_CALENDAR_ID"
+    "lisa": "c196fca542ff5176c62b11805596e015f5fb2aff1ec0b73c43cf555937911638@group.calendar.google.com",
+    "marco": "00edba1ec002f208f25bbd78a7649ef0518121d31071a4fed10ecc0393e3d4b9@group.calendar.google.com",
+    "marie": "5b679eaf3a0999b06265e6b21dd2200800e8b2338056475e1cbd8f17ab34f861@group.calendar.google.com",
+    "max": "1e3a743874cf760bf08026e4fd7eb4a2692c5996e8de1181fd8c87837be524e7@group.calendar.google.com"
 }
 
 WORK_HOURS = {
